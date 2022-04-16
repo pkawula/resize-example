d53cf504c29bd2219c8a48ec64c9ba816085b329
Piotr Kawula <kontakt@piotrkawula.pl>
2022-04-16 13:03:08 +0200

    Prepare example

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react';
+import ResizeDetector, { useResizeDetector } from 'react-resize-detector';
 
 function App() {
+
+    const containerRef = useRef<HTMLDivElement>(null);
+
+    const [sizes, setSizes] = useState<{width: number, height: number}>({ width: 0, height: 0 });
+
+    const { height, width } = useResizeDetector({targetRef: containerRef});
+    
+    const handleResize = (width?: number, height?: number) => {
+    if (width !== undefined && height !== undefined) {
+      setSizes({ width, height });
+    }
+
+    if (width !== undefined)
+      setSizes((prevState) => ({ ...prevState, width }));
+    if (height !== undefined)
+      setSizes((prevState) => ({ ...prevState, height }));
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width:"100%", padding: "20px"}}>
+      <ResizeDetector handleHeight handleWidth targetRef={containerRef} onResize={handleResize}>
+        <div style={{ width: "100%", maxWidth: 400, height: 400, border: "1px solid black" }} ref={containerRef}>
+          <div>
+            <pre>useResizeDetector</pre>
+            <pre>
+              {JSON.stringify({ width, height }, undefined, 4)}
+            </pre>
+          </div>
+          <div>
+            <pre>ResizeDetector</pre>
+            <pre>
+              {JSON.stringify(sizes, undefined, 4)}
+            </pre>
+          </div>
+        </div>
+      </ResizeDetector>
     </div>
   );
 }
